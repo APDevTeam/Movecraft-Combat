6ac630f589e2e6ea36197234e9d7a7ad58a7f36c
TylerS1066 <TylerS1066@users.noreply.github.com>
2020-09-19 12:59:52 -0500

    Cleanup AA director code

--- src/main/java/net/countercraft/movecraft/combat/movecraftcombat/directors/AADirectorManager.java
+++ src/main/java/net/countercraft/movecraft/combat/movecraftcombat/directors/AADirectorManager.java
@@ -30,62 +30,67 @@ public class AADirectorManager extends DirectorManager {
             if (w == null)
                 continue;
             for (SmallFireball fireball : w.getEntitiesByClass(SmallFireball.class)) {
-                if (!(fireball.getShooter() instanceof org.bukkit.entity.LivingEntity)
-                        && w.getPlayers().size() > 0) {
-                    Craft c = CraftManager.getInstance().fastNearestCraftToLoc(fireball.getLocation());
-                    Player p = null;
-                    if (c == null)
-                        continue;
-                    MovecraftLocation midPoint = c.getHitBox().getMidPoint();
-                    int distX = Math.abs(midPoint.getX() - fireball.getLocation().getBlockX());
-                    int distY = Math.abs(midPoint.getY() - fireball.getLocation().getBlockY());
-                    int distZ = Math.abs(midPoint.getZ() - fireball.getLocation().getBlockZ());
-                    boolean inRange = (distX < Config.AADirectorDistance) && (distY < Config.AADirectorDistance) && (distZ < Config.AADirectorDistance);
-                    if (hasDirector(c) && inRange) {
-                        p = getDirector(c);
-                        if (p.getInventory().getItemInMainHand().getType() == Config.DirectorTool) {
-                            Vector fv = fireball.getVelocity();
-                            double speed = fv.length(); // store the speed to add it back in later, since all the values we will be using are "normalized", IE: have a speed of 1
-                            fv = fv.normalize(); // you normalize it for comparison with the new direction to see if we are trying to steer too far
-                            Block targetBlock = p.getTargetBlock(Config.Transparent, Config.AADirectorRange);
-                            Vector targetVector;
-                            if (targetBlock == null) { // the player is looking at nothing, shoot in that general direction
-                                targetVector = p.getLocation().getDirection();
-                            } else { // shoot directly at the block the player is looking at (IE: with convergence)
-                                targetVector = targetBlock.getLocation().toVector().subtract(fireball.getLocation().toVector());
-                                targetVector = targetVector.normalize();
-                            }
-                            if (targetVector.getX() - fv.getX() > 0.5) {
-                                fv.setX(fv.getX() + 0.5);
-                            } else if (targetVector.getX() - fv.getX() < -0.5) {
-                                fv.setX(fv.getX() - 0.5);
-                            } else {
-                                fv.setX(targetVector.getX());
-                            }
-                            if (targetVector.getY() - fv.getY() > 0.5) {
-                                fv.setY(fv.getY() + 0.5);
-                            } else if (targetVector.getY() - fv.getY() < -0.5) {
-                                fv.setY(fv.getY() - 0.5);
-                            } else {
-                                fv.setY(targetVector.getY());
-                            }
-                            if (targetVector.getZ() - fv.getZ() > 0.5) {
-                                fv.setZ(fv.getZ() + 0.5);
-                            } else if (targetVector.getZ() - fv.getZ() < -0.5) {
-                                fv.setZ(fv.getZ() - 0.5);
-                            } else {
-                                fv.setZ(targetVector.getZ());
-                            }
-                            fv = fv.multiply(speed); // put the original speed back in, but now along a different trajectory
-                            fireball.setVelocity(fv);
-                            fireball.setDirection(fv);
-                        }
-                    } else if (inRange) {
-                        p = c.getNotificationPlayer();
-                    }
-                    if (p != null)
-                        fireball.setShooter(p);
+                if (fireball.getShooter() instanceof org.bukkit.entity.LivingEntity || w.getPlayers().size() == 0)
+                    continue;
+
+                Craft c = CraftManager.getInstance().fastNearestCraftToLoc(fireball.getLocation());
+                if (c == null || !hasDirector(c))
+                    continue;
+
+                Player p = getDirector(c);
+
+                MovecraftLocation midPoint = c.getHitBox().getMidPoint();
+                int distX = Math.abs(midPoint.getX() - fireball.getLocation().getBlockX());
+                int distY = Math.abs(midPoint.getY() - fireball.getLocation().getBlockY());
+                int distZ = Math.abs(midPoint.getZ() - fireball.getLocation().getBlockZ());
+                if (distX > Config.AADirectorDistance || distY > Config.AADirectorDistance || distZ > Config.AADirectorDistance)
+                    continue;
+
+                fireball.setShooter(p);
+
+                if (p.getInventory().getItemInMainHand().getType() != Config.DirectorTool)
+                    continue;
+
+                Vector fv = fireball.getVelocity();
+                double speed = fv.length(); // store the speed to add it back in later, since all the values we will be using are "normalized", IE: have a speed of 1
+                fv = fv.normalize(); // you normalize it for comparison with the new direction to see if we are trying to steer too far
+                Block targetBlock = p.getTargetBlock(Config.Transparent, Config.AADirectorRange);
+                Vector targetVector;
+
+                if (targetBlock == null) { // the player is looking at nothing, shoot in that general direction
+                    targetVector = p.getLocation().getDirection();
+                } else { // shoot directly at the block the player is looking at (IE: with convergence)
+                    targetVector = targetBlock.getLocation().toVector().subtract(fireball.getLocation().toVector());
+                    targetVector = targetVector.normalize();
                 }
+
+                if (targetVector.getX() - fv.getX() > 0.5) {
+                    fv.setX(fv.getX() + 0.5);
+                } else if (targetVector.getX() - fv.getX() < -0.5) {
+                    fv.setX(fv.getX() - 0.5);
+                } else {
+                    fv.setX(targetVector.getX());
+                }
+
+                if (targetVector.getY() - fv.getY() > 0.5) {
+                    fv.setY(fv.getY() + 0.5);
+                } else if (targetVector.getY() - fv.getY() < -0.5) {
+                    fv.setY(fv.getY() - 0.5);
+                } else {
+                    fv.setY(targetVector.getY());
+                }
+
+                if (targetVector.getZ() - fv.getZ() > 0.5) {
+                    fv.setZ(fv.getZ() + 0.5);
+                } else if (targetVector.getZ() - fv.getZ() < -0.5) {
+                    fv.setZ(fv.getZ() - 0.5);
+                } else {
+                    fv.setZ(targetVector.getZ());
+                }
+
+                fv = fv.multiply(speed); // put the original speed back in, but now along a different trajectory
+                fireball.setVelocity(fv);
+                fireball.setDirection(fv);
             }
         }
     }
