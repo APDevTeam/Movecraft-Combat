fb4e91c37087e605fdf4d2b33cba7af6639a4015
TylerS1066 <TylerS1066@users.noreply.github.com>
2020-10-31 09:53:13 -0500

    Fixed NPE
    Fixes #35

--- src/main/java/net/countercraft/movecraft/combat/movecraftcombat/fireballs/FireballManager.java
+++ src/main/java/net/countercraft/movecraft/combat/movecraftcombat/fireballs/FireballManager.java
@@ -2,7 +2,6 @@ package net.countercraft.movecraft.combat.movecraftcombat.fireballs;
 
 import net.countercraft.movecraft.combat.movecraftcombat.MovecraftCombat;
 import net.countercraft.movecraft.combat.movecraftcombat.config.Config;
-import net.countercraft.movecraft.combat.movecraftcombat.tracking.FireballTracking;
 import org.bukkit.entity.SmallFireball;
 import org.bukkit.metadata.FixedMetadataValue;
 import org.bukkit.scheduler.BukkitRunnable;
@@ -11,7 +10,7 @@ import java.util.LinkedList;
 
 public class FireballManager extends BukkitRunnable {
     private static FireballManager instance;
-    private LinkedList<SmallFireball> q;
+    private final LinkedList<SmallFireball> q;
 
     public static FireballManager getInstance() {
         return instance;
@@ -26,7 +25,7 @@ public class FireballManager extends BukkitRunnable {
     public void run() {
         int timeLimit = 20 * Config.FireballLifespan * 50;
 
-        while(System.currentTimeMillis() - q.peek().getMetadata("MCC-Expiry").get(0).asLong() > timeLimit && q.size() > 0) {
+        while(q.size() > 0 && System.currentTimeMillis() - q.peek().getMetadata("MCC-Expiry").get(0).asLong() > timeLimit) {
             SmallFireball f = q.pop();
             f.remove();
         }
