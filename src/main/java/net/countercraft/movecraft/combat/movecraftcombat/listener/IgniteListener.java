64f9189272ea8ce014ac0ecf04bb8a0babeb7da5
drfiveminusmint <52045376+drfiveminusmint@users.noreply.github.com>
2020-12-17 03:46:23 -0500

    heckin heckin
    This commit is a testament to why I should not be allowed to write code

--- src/main/java/net/countercraft/movecraft/combat/movecraftcombat/listener/IgniteListener.java
+++ src/main/java/net/countercraft/movecraft/combat/movecraftcombat/listener/IgniteListener.java
@@ -22,6 +22,7 @@ import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockIgniteEvent;
 import org.jetbrains.annotations.NotNull;
