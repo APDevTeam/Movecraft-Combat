8b446087d7f6c29a764445902346a53762595bcb
TylerS1066 <TylerS1066@users.noreply.github.com>
2021-04-10 09:15:32 -0500

    Prototype
    There is duplicate code in getTNT.  It should be replaced with a function which returns a SearchLocation if the tnt is found in the block, or null if not.

--- src/main/java/net/countercraft/movecraft/combat/movecraftcombat/listener/PistonListener.java
+++ src/main/java/net/countercraft/movecraft/combat/movecraftcombat/listener/PistonListener.java
@@ -1,8 +1,8 @@
 package net.countercraft.movecraft.combat.movecraftcombat.listener;
 
 import net.countercraft.movecraft.combat.movecraftcombat.config.Config;
+import net.countercraft.movecraft.utils.Pair;
 import org.bukkit.Location;
-import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Entity;
@@ -13,10 +13,9 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPistonExtendEvent;
 import org.bukkit.event.block.BlockPistonRetractEvent;
 import org.bukkit.event.player.PlayerTeleportEvent;
-import org.bukkit.util.Vector;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-import java.util.ArrayList;
 import java.util.HashSet;
 
 public class PistonListener implements Listener {
@@ -45,16 +44,11 @@ public class PistonListener implements Listener {
         if(!isValidMoveBlock(moveBlock))
             return;
 
+        HashSet<SearchEntry> searchResults = getTNT(piston, pistonHead ? piston.getRelative(direction) : null);
+
         Location moveLoc = getCenterLocation(moveBlock);
-
-        ArrayList<Block> blocks = new ArrayList<>();
-        blocks.add(piston);
-        if(pistonHead)
-            blocks.add(piston.getRelative(direction));
-
-        for(TNTPrimed tnt : getTNTInBlocks(blocks, piston.getWorld())) {
-            tnt.teleport(moveLoc, PlayerTeleportEvent.TeleportCause.PLUGIN);
-            tnt.setVelocity(new Vector());
+        for(SearchEntry se : searchResults) {
+            se.translocateTo(moveLoc);
         }
     }
 
@@ -68,16 +62,16 @@ public class PistonListener implements Listener {
     }
 
     @NotNull
-    private Location getCenterLocation(@NotNull Block moveBlock) {
-        Location moveLoc = new Location(moveBlock.getWorld(), moveBlock.getLocation().getBlockX(), moveBlock.getLocation().getBlockY(), moveBlock.getLocation().getBlockZ());
-        moveLoc.add(0.5D, 0.5D, 0.5D);
-        return moveLoc;
+    private Location getCenterLocation(@NotNull Block block) {
+        Location loc = new Location(block.getWorld(), block.getLocation().getBlockX(), block.getLocation().getBlockY(), block.getLocation().getBlockZ());
+        loc.add(0.5D, 0.5D, 0.5D);
+        return loc;
     }
 
     @NotNull
-    private HashSet<TNTPrimed> getTNTInBlocks(@NotNull ArrayList<Block> blocks, @NotNull World w) {
-        HashSet<TNTPrimed> tntSet = new HashSet<>();
-        for(Entity e : w.getEntities()) {
+    private HashSet<SearchEntry> getTNT(@NotNull Block piston, @Nullable Block pistonHead) {
+        HashSet<SearchEntry> searchResults = new HashSet<>();
+        for(Entity e : piston.getWorld().getEntities()) {
             if(!e.isValid() || e.getType() != EntityType.PRIMED_TNT)
                 continue;
 
@@ -86,20 +80,49 @@ public class PistonListener implements Listener {
                 continue;
 
             Location loc = tnt.getLocation();
-            for(Block b : blocks) {
-                if(isInBlock(loc, b)) {
-                    tntSet.add(tnt);
-                    break;
-                }
+            if(isInBlock(loc, piston)) {
+                Pair<Double, Double> offsets = getOffset(loc, getCenterLocation(piston));
+                if(isInBlock(offsets))
+                    searchResults.add(new SearchEntry(tnt, offsets.getLeft(), offsets.getRight()));
+            }
+            else if(pistonHead != null && isInBlock(loc, pistonHead)) {
+                Pair<Double, Double> offsets = getOffset(loc, getCenterLocation(pistonHead));
+                if(isInBlock(offsets))
+                    searchResults.add(new SearchEntry(tnt, offsets.getLeft(), offsets.getRight()));
             }
         }
-
-        return tntSet;
+        return searchResults;
     }
 
-    private boolean isInBlock(@NotNull Location l, @NotNull Block b) {
-        return l.getBlockX() == b.getLocation().getBlockX()
-                && l.getBlockY() == b.getLocation().getBlockY()
-                && l.getBlockZ() == b.getLocation().getBlockZ();
+    private boolean isInBlock(@NotNull Location loc, @Nullable Block b) {
+        return loc.getBlock() != b;
+    }
+
+    @NotNull
+    private Pair<Double, Double> getOffset(@NotNull Location one, @NotNull Location two) {
+        double xOffset = one.getX() - two.getX();
+        double zOffset = one.getZ() - two.getZ();
+        return new Pair<>(xOffset, zOffset);
+    }
+
+    private boolean isInBlock(@NotNull Pair<Double, Double> offsets) {
+        return Math.abs(offsets.getLeft()) < 0.021 && Math.abs(offsets.getRight()) < 0.021;
+    }
+
+    private static class SearchEntry {
+        private final TNTPrimed tnt;
+        private final double xOffset;
+        private final double zOffset;
+
+        public SearchEntry(TNTPrimed tnt, double xOffset, double zOffset) {
+            this.tnt = tnt;
+            this.xOffset = xOffset;
+            this.zOffset = zOffset;
+        }
+
+        public void translocateTo(@NotNull Location loc) {
+            Location moveLoc = new Location(loc.getWorld(), loc.getX() + xOffset, loc.getY(), loc.getZ() + zOffset);
+            tnt.teleport(moveLoc, PlayerTeleportEvent.TeleportCause.PLUGIN);
+        }
     }
 }
