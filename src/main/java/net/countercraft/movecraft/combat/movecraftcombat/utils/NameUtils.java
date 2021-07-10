1dbe481e0668f0786f53ac3210dd1ab4f8b3d55c
TylerS1066 <TylerS1066@users.noreply.github.com>
2021-07-10 17:31:28 -0500

    Update NameUtils.java

--- src/main/java/net/countercraft/movecraft/combat/movecraftcombat/utils/NameUtils.java
+++ src/main/java/net/countercraft/movecraft/combat/movecraftcombat/utils/NameUtils.java
@@ -1,6 +1,7 @@
 package net.countercraft.movecraft.combat.movecraftcombat.utils;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
@@ -9,7 +10,7 @@ public class NameUtils {
     public static String offlineToName(@NotNull OfflinePlayer offlinePlayer) {
         Player p = Bukkit.getPlayer(offlinePlayer.getUniqueId());
         if(p != null)
-            return p.getDisplayName();
+            return p.getDisplayName() + ChatColor.RESET;
 
         return offlinePlayer.getName();
     }
