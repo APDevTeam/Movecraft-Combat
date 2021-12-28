44271d08f38f639401eb16477594a9179d29299a
TylerS1066 <TylerS1066@users.noreply.github.com>
2021-12-28 14:44:35 -0600

    Update CombatEvent.java

--- src/main/java/net/countercraft/movecraft/combat/features/combat/events/CombatEvent.java
+++ src/main/java/net/countercraft/movecraft/combat/features/combat/events/CombatEvent.java
@@ -1,13 +1,18 @@
 package net.countercraft.movecraft.combat.features.combat.events;
 
-import org.jetbrains.annotations.Nullable;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
+import org.jetbrains.annotations.Nullable;
 
 
 public class CombatEvent extends Event {
     private static final HandlerList HANDLERS = new HandlerList();
+    private final Player player;
+
+    public CombatEvent(@Nullable Player player) {
+        this.player = player;
+    }
 
     @SuppressWarnings("unused")
     public static HandlerList getHandlerList() {
@@ -18,16 +23,6 @@ public class CombatEvent extends Event {
     public HandlerList getHandlers() {
         return HANDLERS;
     }
-
-
-
-    private final Player player;
-
-
-    public CombatEvent(@Nullable Player player) {
-        this.player = player;
-    }
-
 
     @Nullable
     public Player getPlayer() {
