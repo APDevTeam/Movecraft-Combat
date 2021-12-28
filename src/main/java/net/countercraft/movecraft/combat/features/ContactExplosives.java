c7d7f967817b756ec8b88fd27fa17bb815bad0a2
TylerS1066 <TylerS1066@users.noreply.github.com>
2021-12-28 13:11:29 -0600

    Implement a min speed for contact explosives and fix transparent blocks

--- src/main/java/net/countercraft/movecraft/combat/features/ContactExplosives.java
+++ src/main/java/net/countercraft/movecraft/combat/features/ContactExplosives.java
@@ -16,12 +16,12 @@ import org.jetbrains.annotations.NotNull;
 public class ContactExplosives extends BukkitRunnable implements Listener {
     public static boolean EnableContactExplosives = true;
     public static double ContactExplosivesMaxImpulseFactor = 10;
-    public static int ContactExplosivesDelay = 10;
+    public static double ContactExplosivesMinImpuse = 0.35;
 
     public static void load(@NotNull FileConfiguration config) {
         EnableContactExplosives = config.getBoolean("EnableContactExplosives", true);
         ContactExplosivesMaxImpulseFactor = config.getDouble("ContactExplosivesMaxImpulseFactor", 10.0);
-        ContactExplosivesDelay = 80 - (int) (20 * config.getDouble("ContactExplosivesDelay", 0.5D));
+        ContactExplosivesMinImpuse = config.getDouble("ContactExplosivesMinImpuse", 0.35);
     }
 
 
@@ -32,40 +32,34 @@ public class ContactExplosives extends BukkitRunnable implements Listener {
 
     @Override
     public void run() {
-        if(!EnableContactExplosives)
+        if (!EnableContactExplosives)
             return;
 
         long ticksElapsed = (System.currentTimeMillis() - lastCheck) / 50;
-        if(ticksElapsed <= 0)
+        if (ticksElapsed <= 0)
             return;
 
         // see if there is any new rapid moving TNT in the worlds
-        for(World w : Bukkit.getWorlds()) {
-            if(w == null || w.getPlayers().size() == 0)
+        for (World w : Bukkit.getWorlds()) {
+            if (w == null || w.getPlayers().size() == 0)
                 continue;
 
             var allTNT = w.getEntitiesByClass(TNTPrimed.class);
-            for(TNTPrimed tnt : allTNT) {
-                if(tnt.getFuseTicks() < ContactExplosivesDelay && !tracking.containsKey(tnt)) {
-                    Bukkit.getLogger().info("Putting " + tnt.getLocation() + " : " + tnt.getVelocity().lengthSquared() + " w/ fuse " + tnt.getFuseTicks());
+            for (TNTPrimed tnt : allTNT) {
+                if (tnt.getVelocity().lengthSquared() <= ContactExplosivesMinImpuse && !tracking.containsKey(tnt))
                     tracking.put(tnt, tnt.getVelocity().lengthSquared());
-                }
-                else if(!tracking.containsKey(tnt))
-                    Bukkit.getLogger().info("Skipping " + tnt.getLocation() + " : " + tnt.getVelocity().lengthSquared() + " w/ fuse " + tnt.getFuseTicks());
             }
         }
 
         // now check to see if any has abruptly changed velocity, and should explode
-        for(TNTPrimed tnt : tracking.keySet()) {
+        for (TNTPrimed tnt : tracking.keySet()) {
             double vel = tnt.getVelocity().lengthSquared();
-            if(vel < tracking.getDouble(tnt) / ContactExplosivesMaxImpulseFactor) {
-                Bukkit.getLogger().info("Esploding: " + tnt.getLocation() + ": " + vel + " / " + tracking.getDouble(tnt));
-                tnt.setVelocity(new Vector(0,0,0)); //freeze it in place to prevent sliding
+            if (vel < tracking.getDouble(tnt) / ContactExplosivesMaxImpulseFactor) {
+                tnt.setVelocity(new Vector(0, 0, 0)); //freeze it in place to prevent sliding
                 tnt.setFuseTicks(0);
             }
             else {
-                // update the tracking with the new velocity so gradual
-                // changes do not make TNT explode
+                // update the tracking with the new velocity so gradual changes do not make TNT explode
                 tracking.put(tnt, vel);
             }
         }
@@ -79,9 +73,9 @@ public class ContactExplosives extends BukkitRunnable implements Listener {
 
     @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
     public void onEntityExplode(@NotNull EntityExplodeEvent e) {
-        if(!EnableContactExplosives)
+        if (!EnableContactExplosives)
             return;
-        if(!(e.getEntity() instanceof TNTPrimed))
+        if (!(e.getEntity() instanceof TNTPrimed))
             return;
 
         tracking.removeDouble(e.getEntity());
