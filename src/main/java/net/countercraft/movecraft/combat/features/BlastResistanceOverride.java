41ac599b544dc081be544009f81b3c6ec9bb6307
TylerS1066 <TylerS1066@users.noreply.github.com>
2023-02-12 08:56:54 -0600

    Start laying out architecture of feature

--- src/main/java/net/countercraft/movecraft/combat/features/BlastResistanceOverride.java
+++ src/main/java/net/countercraft/movecraft/combat/features/BlastResistanceOverride.java
@@ -1,9 +1,11 @@
 package net.countercraft.movecraft.combat.features;
 
+import java.lang.reflect.Field;
 import java.util.EnumSet;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.commons.lang.reflect.FieldUtils;
 import org.bukkit.Material;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.jetbrains.annotations.NotNull;
@@ -27,5 +29,23 @@ public class BlastResistanceOverride {
                 BlastResistanceOverride.put(m, (Float) entry.getValue());
             }
         }
+    }
+
+    private static Field getField() {
+        return FieldUtils.getField(Material.class, "durability");
+    }
+
+    public static boolean setBlastResistance(Material m, float resistance) {
+        try {
+            // FieldUtils.writeField(getField(), m, resistance);
+        } catch (IllegalAccessError e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean revertToVanilla(Material m) {
+        return setBlastResistance(m, m.getBlastResistance()); // Spigot stores the vanilla values for us
     }
 }
